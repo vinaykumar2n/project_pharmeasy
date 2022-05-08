fa9f3a4477c284752bdcb8804acd3163b0158cf8
vinaykumar2n <naravinaykumar2@gmail.com>
2022-05-08 13:29:12 +0530

    completed product and cart pages

---
+++ cartvinayandshubham/index.js
@@ -0,0 +1,188 @@
+
+// nav bar 
+ // Get the modal
+ var modal = document.getElementById("myModal");
+ var modal2 = document.getElementById("myModal2");
+
+ // Get the button that opens the modal
+ var btn = document.getElementById("myBtn");
+ var btn2 = document.getElementById("myBtn2");
+
+ // Get the <span> element that closes the modal
+ var span = document.getElementsByClassName("close")[0];
+ var span2 = document.getElementsByClassName("close2")[0];
+
+ // When the user clicks the button, open the modal
+ btn.onclick = function () {
+   modal.style.display = "block";
+ };
+
+ // When the user clicks on <span> (x), close the modal
+ span.onclick = function () {
+   modal.style.display = "none";
+ };
+
+ // When the user clicks anywhere outside of the modal, close it
+ window.onclick = function (event) {
+   if (event.target == modal) {
+     modal.style.display = "none";
+   }
+ };
+
+ btn2.onclick = function () {
+   modal2.style.display = "block";
+ };
+
+ // When the user clicks on <span> (x), close the modal
+ span2.onclick = function () {
+   modal2.style.display = "none";
+ };
+
+ // When the user clicks anywhere outside of the modal, close it
+ window.onclick = function (event) {
+   if (event.target == modal2) {
+     modal2.style.display = "none";
+   }
+ };
+// End of Nav bar---------------------------------------------------------------------
+
+var cartData=JSON.parse(localStorage.getItem("cartData")) || []
+
+var cartquantity= JSON.parse(localStorage.getItem("cartquantity")) || []
+
+display(cartData)
+  function display(items){
+    document.querySelector("#cartitems").innerHTML=""
+    items.map(function(el,i){
+      var card=document.createElement("div");
+      card.style.display="flex"
+      card.setAttribute("class","card")
+      
+      var imagediv=document.createElement("div")
+      imagediv.setAttribute("class","imgdiv");
+
+      var image=document.createElement("img");
+      image.setAttribute("src",el.image_url);
+      image.setAttribute("class","realimg")
+
+      imagediv.append(image)
+
+      var besideimage=document.createElement("div");
+      besideimage.setAttribute("id","besideimage")
+
+      var name=document.createElement("h4");
+      name.innerText=el.name;
+      name.setAttribute("id","pname")
+
+      var mfgby=document.createElement("h4");
+      mfgby.innerText=el.mfgby;
+      mfgby.setAttribute("id","pmfgby")
+
+      var description=document.createElement("h4");
+      description.innerText=el.description;
+      description.setAttribute("id","pdescription")
+
+
+      var pricediv=document.createElement("div");
+      pricediv.setAttribute("id","qandpricediv")
+      pricediv.style.display="flex"
+      pricediv.style.alignItems="center"
+
+      var qty=document.createElement("select")
+      qty.setAttribute("id","selecttagqty")
+      qty.innerHTML= `<option value="none">Qty:</option>
+                      <option value="1">1</option>
+                      <option value="2">2</option>
+                      <option value="3">3</option>
+                      <option value="4">4</option>
+                      <option value="5">5</option>
+                      <option value="6">6 <span>Max Qty</span></option>`
+      
+    qty.addEventListener("change",function(){
+        qtychange(el,qty)
+    })
+
+      var actualquantity=document.createElement("p");
+      actualquantity.setAttribute("id","displayquant")
+      actualquantity.innerText=el.quantity
+      
+      var price=document.createElement("h2");
+      price.innerHTML=`Rs.`  
+      var amount=document.createElement("span");
+      amount.innerText=(el.price*el.quantity).toFixed(2)
+      price.append(amount)
+
+
+      var strike=document.createElement("h4");
+      strike.innerHTML=`Rs.`;
+      strike.style.textDecoration="line-through"
+      strike.style.marginLeft="30px"
+
+      var st=document.createElement("span");
+      st.innerText=(el.strikedoffprice*el.quantity).toFixed(2)
+      strike.append(st)
+
+      var delivery=document.createElement("div");
+      delivery.setAttribute("id","delivery")
+      delivery.innerHTML="<p>Delivery by <span>Tomorrow, before 10pm<span></p>"
+
+      
+      var del=document.createElement("div")
+      del.setAttribute("id","del")
+      var deletebtn=document.createElement("img");
+      deletebtn.setAttribute("id","delimg")
+      deletebtn.setAttribute("src","https://assets.pharmeasy.in/web-assets/dist/2fb50086.svg")
+      
+      deletebtn.addEventListener("click",function(){
+        deletefun(el,i)
+      })
+      del.append(deletebtn)
+
+      pricediv.append(qty,actualquantity,price,strike);
+
+      besideimage.append(name,mfgby,description,pricediv,delivery)
+
+      card.append(imagediv,besideimage,del);
+
+      document.querySelector("#cartitems").append(card)
+    })
+  }
+
+
+
+function deletefun(el,i){
+  cartData.splice(i,1)
+  localStorage.setItem("cartData",JSON.stringify(cartData))
+  window.location.reload()
+}
+function qtychange(el,qty){
+  el.quantity=qty.value;
+  localStorage.setItem("cartData",JSON.stringify(cartData))
+  cartData=JSON.parse(localStorage.getItem("cartData")) || []
+  display(cartData)
+  window.location.reload();
+}
+var total= cartData.reduce(function(acc,el){
+  return acc+(el.price*el.quantity)
+},0)
+var strikedtotal=cartData.reduce(function(acc,el){
+  return acc+(el.strikedoffprice*el.quantity)
+},0)
+total=total.toFixed(2)
+strikedtotal=strikedtotal.toFixed(2)
+localStorage.setItem("totalamt",total);
+localStorage.setItem("strikedtotal",strikedtotal);
+
+document.querySelector("#cartvalueprice").innerText=total
+document.querySelector("#cartvaluestriked").innerText=strikedtotal
+document.querySelector("#num").innerText=cartData.length
+
+if(cartData.length>0){
+  document.querySelector("#deliverycharge").innerText=84.03
+  document.querySelector("#amounttobepaid").innerText=(Number(total)+84.03).toFixed(2)
+  document.querySelector("#saved").innerText=(strikedtotal-total).toFixed(2)
+  document.querySelector("#discounted").innerText=(Number(total)*0.1798).toFixed(2)
+}
+
+
+
---
+++ productvinay/aftersearchpage.js
@@ -0,0 +1,126 @@
+var searchedItem=JSON.parse(localStorage.getItem("searchedItem"))
+    display(searchedItem);
+
+    function display(data){
+    document.querySelector("#pronameafterhome").innerText=data.name
+    document.querySelector("title").innerText=data.name
+    var imgbox=document.createElement("div")
+    imgbox.setAttribute("id","imgbox")
+
+    var image=document.createElement("img");
+    image.setAttribute("src",data.image_url)
+    image.style.maxHeight="100%"
+    image.style.maxWidth="100%"
+
+    imgbox.append(image);
+    document.querySelector("#proddisplay").append(imgbox)
+
+    // appended image to imgbox and imgbox to #proddisplay
+
+    var prodbox=document.createElement("div");
+    prodbox.setAttribute("id","prodbox")
+    
+    var prodname=document.createElement("h3");
+    prodname.innerText=data.name;
+    prodname.setAttribute("id","Pname")
+
+    var prodmfg=document.createElement("p");
+    prodmfg.innerText=data.mfgby;
+    prodmfg.setAttribute("id","Pmfg")
+
+    var proddes=document.createElement("p");
+    proddes.innerText=data.description;
+    proddes.setAttribute("id","Pdes");
+
+    var pricediv=document.createElement("div")
+    pricediv.setAttribute("id","pricediv")
+    
+    
+       
+    
+    var price=document.createElement("div");
+
+    var amount=document.createElement("span");
+    amount.innerText=`Rs.` 
+    var prodprice=document.createElement("p");
+    prodprice.innerText=data.price;
+    prodprice.setAttribute("id","Pprice")
+    price.append(amount,prodprice)
+    
+
+    var prodstriked=document.createElement("p");
+    prodstriked.innerText=data.strikedoffprice;
+    prodstriked.setAttribute("id","Pstriked")
+
+    pricediv.append(price,prodstriked)
+    
+    var tax=document.createElement("p");
+    tax.innerText="Inclusive of all taxes";
+    tax.setAttribute("id","Ptax")
+
+    var cartandqtydiv=document.createElement("div");
+    cartandqtydiv.setAttribute("id","cartandqty")
+
+    var cartbutton=document.createElement("button");
+    cartbutton.innerText="Add to Cart";
+    cartbutton.setAttribute("id","cartbtn");
+    cartbutton.addEventListener("click",function(){
+        addingtocart(data)
+    })
+
+    var qty=document.createElement("select")
+    qty.setAttribute("id","filterQuantity")
+    qty.innerHTML= `<option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6 <span>Max Qty</span></option>`
+    qty.addEventListener("change",function(){
+        qtychange(data,qty)
+    })
+
+    var delivery=document.createElement("div");
+    delivery.setAttribute("id","delivery")
+    delivery.innerHTML="<p>Delivery by <span>Tomorrow, before 10pm<span></p>"
+    cartandqtydiv.append(cartbutton,qty)
+    prodbox.append(prodname,prodmfg,proddes,pricediv,tax,cartandqtydiv,delivery)
+    document.querySelector("#proddisplay").append(prodbox)
+}
+
+var cartData=JSON.parse(localStorage.getItem("cartData")) ||[];
+
+function qtychange(data,qty){
+    data.quantity=qty.value;
+}
+
+var c
+function addingtocart(data){
+    c=cartData.find(function(ele){
+        return data.uniqueid==ele.uniqueid
+    })
+    console.log(c)
+    if(c)
+    {
+        alert("Already added to cart")
+    }
+    else{
+        cartData.push(data);
+        localStorage.setItem("cartData",JSON.stringify(cartData))
+        document.querySelector("#additemdisplay button").style.backgroundColor="rgb(16,132,126)"
+        document.querySelector("#additemdisplay>div").innerHTML=`<span></span><p>Item(s) in Cart</p>`
+        document.querySelector("#additemdisplay>div>span").innerText=cartData.length
+    }  
+
+}
+
+if(cartData.length>0)
+{
+    document.querySelector("#additemdisplay button").style.backgroundColor="rgb(16,132,126)"
+        document.querySelector("#additemdisplay>div").innerHTML=`<span></span><p>Item(s) in Cart</p>`
+        document.querySelector("#additemdisplay>div>span").innerText=cartData.length
+}
+
+function viewcartfun(){
+    window.location.href="index.html"
+}
